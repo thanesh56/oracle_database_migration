525b8b04b1fb6f0c178611bf55af28ae8962aea6
thaneshwar sahu <thaneshwar.sahu@globallogic.com>
2020-01-01 10:08:35 +0530

    add code of database migration

---
+++ src/main/java/com/gl/oracle/databasemigration/DatabasemigrationApplication.java
@@ -0,0 +1,13 @@
+package com.gl.oracle.databasemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasemigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasemigrationApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -0,0 +1,1 @@
+create table c##ender.user_tb as (select * from system.user_tb);
